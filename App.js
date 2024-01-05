89182b535961f305674c085cb6eeaa17e85d76dd
KoheiUnoki <148199939+KoheiUnoki@users.noreply.github.com>
2024-01-05 12:03:04 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import NewsArticles from "./components/NewsArticles";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URI = `https://newsapi.org/v2/top-headlines?country=jp&category=entertainment&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export default function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    const response = await axios.get(URI);
+    setNews(response.data.articles);
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={news}
+        renderItem={({ item }) => (
+          <NewsArticles
+            imageuri={item.urlToImage}
+            title={item.title}
+            subtext={item.publishedAt}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
